97f270d9b4f984bde99868f74170cfbb708debfd
akuraru <akuraru@gmail.com>
2013-12-01 16:43:29 +0900

    ファイルを追加

---
+++ IntroductionOfStoryboardForTableView/MainStoryboardEntry.h
@@ -0,0 +1,13 @@
+//
+//  MainStoryboardEntry.h
+//  IntroductionOfStoryboardForTableView
+//
+//  Created by akuraru on 2013/12/01.
+//  Copyright (c) 2013年 akuraru. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MainStoryboardEntry : NSObject
+
+@end
---
+++ IntroductionOfStoryboardForTableView/MainStoryboardEntry.m
@@ -0,0 +1,13 @@
+//
+//  MainStoryboardEntry.m
+//  IntroductionOfStoryboardForTableView
+//
+//  Created by akuraru on 2013/12/01.
+//  Copyright (c) 2013年 akuraru. All rights reserved.
+//
+
+#import "MainStoryboardEntry.h"
+
+@implementation MainStoryboardEntry
+
+@end
